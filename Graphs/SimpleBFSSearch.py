f9d2b92066fe6ce34645f55ac403fea978049303
Susan Bhattarai <susan.bhattarai@bison.howard.edu>
2016-10-08 22:30:16 -0400

    Graphs Intro:

---
+++ Graphs/SimpleBFSSearch.py
@@ -0,0 +1,60 @@
+class Queue:
+    def __init__(self):
+        self.old = []
+        self.new = []
+    
+    def size(self):
+        return len(self.old) + len(self.new)
+
+    def enqueue(self, val):
+        self.new.append(val)
+    
+    def shiftStack(self):
+        if len(self.old) == 0:
+            while len(self.new) > 0:
+                self.old.append(self.new.pop())
+        
+    def peek(self):
+        self.shiftStack()
+        return self.old[-1]
+    
+    def dequeue(self):
+        self.shiftStack()
+        return self.old.pop()
+    
+    def empty(self):
+        return self.size() == 0
+
+graph = {'A':['B', 'C'],
+         'B': ['A', 'D', 'E'],
+         'C': ['A', 'F'],
+         'D': ['B'],
+         'E': ['B', 'F'],
+         'F': ['C', 'E']}
+
+def searchGraphBFS(graph, start, end):
+    if start == end:
+        return True
+    visited = set();
+    bfsQueue  = Queue()
+    visited.add(start)
+    bfsQueue.enqueue(start)
+    while not bfsQueue.empty():
+        currentNode = bfsQueue.dequeue()
+        if currentNode:
+            for nodes in graph[currentNode]:
+                if nodes not in visited:
+                    if nodes == end:
+                        return True
+                    else:
+                        bfsQueue.enqueue(nodes)
+                        visited.add(nodes)
+    return False
+
+
+print searchGraphBFS(graph, 'A', 'Z')
+
+
+        
+    
+                        
